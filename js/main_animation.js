49c633a46cb6d7d533f0c70819f1fa561fa3c650
LausMirus <nripanofficial@gmail.com>
2026-01-28 19:24:24 +0530

    added js

--- js/main_animation.js
+++ js/main_animation.js
@@ -1,17 +1,28 @@
 gsap.to("#page1_content #title",{
-    scaleX:100,
-    duration:3,
+    // transform:"translateX(2048px)",
+    z:100,
+    duration:1,
+    opacity:0,
+    scale:10,
     scrollTrigger:{
-        trigger:"#page1_content",
+        trigger:"#page1_content #title",
         scroller:"body",
         markers:true,
-        start:"top 20%",
-        end:"top 30%",
+        start:"top 25%",
+        end:"top 5%",
         scrub:2,
     }
-
 })
-// gsap.from("#page1_content #title",{
-//     duration:1,
-//     transform:"translateX(-1000px)"
-// })
+gsap.to("#page1_content #sub_title",{
+    duration:.5,
+    opacity:0,
+    // transform:"translateX(-1000px)"]
+    scrollTrigger:{
+        trigger:"#page1_content #sub_title",
+        scroller:"body",
+        marker:true,
+        start:"top 25%",
+        end:"top 20%",
+        scrub:2,
+    }
+})
